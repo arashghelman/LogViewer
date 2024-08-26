c3bf97adcb055e352a6e71d444c418774cd2c2c9
Arash Ghelman <arashghelman78@gmail.com>
2024-08-26 17:04:24 +0330

    Add project files.

---
+++ LogViewer.Console/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.RegularExpressions;
+
+var path = @"D:\project\LogViewer\tart-logs-app_20240821.log";
+var pattern = @"\{[^}]+\}";
+
+using var reader = new StreamReader(path);
+var fileContent = await reader.ReadToEndAsync();
+
+var matches = Regex.Matches(fileContent, pattern);
+var jsonLogs = new List<string>();
+foreach (Match match in matches)
+{
+    jsonLogs.Add(match.Value);
+}
+
+var b = 02;
