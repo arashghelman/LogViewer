a8b5e7e7fe1c57f8d56dccb515d9e0fc8da0a0c9
Arash Ghelman <arashghelman78@gmail.com>
2024-08-27 16:21:12 +0330

    Add string logs to JsonObject conversion logic

--- LogViewer.Console/Program.cs
+++ LogViewer.Console/Program.cs
@@ -1,16 +1,28 @@
 ﻿using System.Text.RegularExpressions;
+using System.Text.Json.Nodes;
 
 var path = @"D:\project\LogViewer\tart-logs-app_20240821.log";
-var pattern = @"\{[^}]+\}";
+var pattern = @"\{(?:[^{}]|(?<Open>\{)|(?<-Open>\}))+(?(Open)(?!))\}";
 
 using var reader = new StreamReader(path);
-var fileContent = await reader.ReadToEndAsync();
+var logsString = await reader.ReadToEndAsync();
 
-var matches = Regex.Matches(fileContent, pattern);
-var jsonLogs = new List<string>();
+var jsonObjects = new List<JsonObject>();
+
+var matches = Regex.Matches(logsString, pattern);
+
 foreach (Match match in matches)
 {
-    jsonLogs.Add(match.Value);
+    var jsonObject = JsonNode.Parse(match.Value)?.AsObject();
+
+    foreach (var property in jsonObject)
+    {
+        if (property.Value is JsonObject nestedObject)
+        {
+        }
+    }
+
+    jsonObjects.Add(jsonObject);
 }
 
 var b = 02;
