228053c49e1365f3327af1511f0d2f3c561dac30
arashghelman <arashghelman77@gmail.com>
2025-05-23 18:05:46 +0330

    Create UI app

--- LogViewer.Console/Program.cs
+++ LogViewer.Console/Program.cs
@@ -1,38 +1,10 @@
-﻿using System.Text.RegularExpressions;
-using System.Text.Json;
+﻿using LogViewer.Core.Parsers;
 
-var path = @"D:\Logs\logs.log";
-var pattern = @"\{(?:[^{}]|(?<Open>\{)|(?<-Open>\}))+(?(Open)(?!))\}";
-
+var path = "/Users/arashghelman/Projects/LogViewer/nginx_json_logs.txt";
 using var reader = new StreamReader(path);
 var fileText = await reader.ReadToEndAsync();
 
-var parsedLogs = new List<List<KeyValuePair<string, string>>>();
+var logParser = new JsonLogParser();
+var logs = logParser.Parse(fileText);
 
-var matches = Regex.Matches(fileText, pattern);
-
-foreach (Match match in matches)
-{
-    var fields = new List<KeyValuePair<string, string>>();
-
-    var jsonElement = JsonSerializer.Deserialize<JsonElement>(match.Value);
-
-    FlattenJson(jsonElement, fields);
-
-    parsedLogs.Add(fields);
-}
-
-void FlattenJson(JsonElement element, List<KeyValuePair<string, string>> fields)
-{
-    foreach (var property in element.EnumerateObject())
-    {
-        if (property.Value.ValueKind is JsonValueKind.Object)
-        {
-            FlattenJson(property.Value, fields);
-        }
-        else
-        {
-            fields.Add(new KeyValuePair<string, string>(property.Name, property.Value.ToString()));
-        }
-    }
-}
+var a = 20;
--- LogViewer/Parsers/ILogParser.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Text.Json.Nodes;
-
-namespace LogViewer.Parsers;
-
-public interface ILogParser
-{
-    Task<List<JsonObject>> Parse(string text);
-}
